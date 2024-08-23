1abe6f891707c8b3ab3a170bfdf7e663bec4da35
imsachinkumar <122716893+imsachinkumar@users.noreply.github.com>
2024-08-23 11:04:33 +0000

    primitive_and_obj

---
+++ 01_js_tutorial/02_primitive_and_obj.js
@@ -0,0 +1,29 @@
+
+
+//PRIMITIVE DATATYPES
+
+//nn bb ss u
+//number,null,boolean,big int,symbol,string,undefined
+
+let a=567
+let b=null
+let c=true;
+let d=BigInt("567")
+let e=Symbol("i m symbol")
+let f="sachin"
+let g="undefined"
+
+console.log(a,b,c,d,e,f,g)
+
+console.log(typeof(a));
+
+//OBJECT(NON PRIMITIVE DATATYPES);
+
+const item={
+    "Sachin":true,
+    "suresh":false,
+    "Ram":null,
+    "shyam":67
+}
+
+console.log(item["Ram"])
