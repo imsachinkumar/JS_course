0f7f8e24425b28f324dc1304ee40957682fe2906
imsachinkumar <122716893+imsachinkumar@users.noreply.github.com>
2024-08-23 13:41:16 +0000

    operators

---
+++ 01_js_tutorial/04_operators.js
@@ -0,0 +1,47 @@
+
+
+//OPERATORS AND EXPRESSIONS
+
+let a=20
+let b=3
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+
+console.log(a++)//yaha increment hoga pehle then console.log(a) dubara likhne prr 21 print hoga
+console.log(a)
+console.log(++a)//yaha pehle incrment hoke print ho jaega
+console.log(b++)
+console.log(b)
+
+console.log(a--)
+console.log(a)
+
+//ASSIGNMENT OPERATORS
+
+let c=43;
+c+=2
+console.log(c)
+
+let d=12
+d-=3
+console.log(d)
+d*=2
+console.log(d)
+d/=5
+console.log(d)
+
+//COMPARISON OPERATORS
+
+comp1=14
+comp2=13
+console.log(comp1==comp2)
+
+//LOGICAL OPERATOR
+
+let f=12
+let g=13
+console.log(f<g && f==12)
