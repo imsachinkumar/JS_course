65663e55ee1714322111219efc5fa51b3dc072dd
imsachinkumar <122716893+imsachinkumar@users.noreply.github.com>
2024-08-23 11:29:38 +0000

    practice

---
+++ 01_js_tutorial/03_practice_set1.js
@@ -0,0 +1,38 @@
+//Q1
+//add string with number
+
+let a="Sachin"
+let b=57
+console.log(a+b)
+
+//Q2
+//use opertaor to find datatype of above 
+
+console.log(typeof(a))
+console.log(typeof(b))
+
+//Q3
+//create a const and check can you chnage its value
+
+const c={
+    name:"shyam",
+    section:1
+}
+
+//no we cant change
+
+//Q4 adding a new key in q3
+c['college']="GLA"
+console.log(c)
+
+//Q5
+//creating a dictionary
+
+const dict={
+    approve:"o be pleased about something;",
+    reject:"to refuse to accept somebody/something",
+    remove:"to take somebody something off or away"
+}
+console.log(dict["approve"])
+      //OR
+console.log(dict.approve)
