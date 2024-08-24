e3b3e8f0bae737d347d8944ada257e04fc5545e4
imsachinkumar <122716893+imsachinkumar@users.noreply.github.com>
2024-08-24 13:59:48 +0000

    func

---
+++ 01_js_tutorial/10_function.js
@@ -0,0 +1,22 @@
+// FUNCTION IS A BLOCK OF CODE DESIGNED TO PERFORM A PARTICULAR TASK
+
+function sum(x,y){
+    return x+y
+}
+let x=3;
+let y=7;
+
+console.log(sum(x,y));
+
+//NOTE
+// Math.round is used to print round of value.
+
+//new method of creating function
+
+const subtract=(p,q)=>{
+    return p-q
+}
+let p=67
+let q=10
+
+console.log(subtract(p,q))
