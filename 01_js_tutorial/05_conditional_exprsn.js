7ac5e90e9b0b01f26ddbe977620c8fba6db4020b
imsachinkumar <122716893+imsachinkumar@users.noreply.github.com>
2024-08-24 05:41:40 +0000

    ifelse

--- 01_js_tutorial/05_conditional_exprsn.js
+++ 01_js_tutorial/05_conditional_exprsn.js
@@ -1,1 +1,33 @@
+//PROMPT AND ALERT
+
+// let a=prompt("what is your age?")
+// //console.log(typeof(a))
+// a=Number.parseInt(a)
+// console.log(typeof(a));
+
+//IF STATEMENT
+
+let a=3
+if(a>23){
+    console.log("true");
+}else{
+    console.log("false");
+}
+
+//IF ELSE IF ELSE STATEMENT
+
+let age=54;
+if(age>20){
+    console.log("graeter");
+
+}else if(age<20){
+
+  console.log("smaller");
+
+}
+
+else{
+    console.log("equal")
+
+}
 
