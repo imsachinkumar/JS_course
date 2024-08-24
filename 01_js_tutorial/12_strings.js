9b1180348c00fea30346f1e79486725887824ca5
imsachinkumar <122716893+imsachinkumar@users.noreply.github.com>
2024-08-24 16:56:43 +0000

    string

---
+++ 01_js_tutorial/12_strings.js
@@ -0,0 +1,23 @@
+//STRINGS
+
+// let str="chaudhary"
+// console.log(str.length)
+
+
+// console.log(str[0])
+// console.log(str[4])
+
+// for(let i=0;i<str.length;i++){
+//     console.log(str[i])
+// }
+
+
+
+//template literal
+
+
+let a="hello"
+let b ="world"
+
+let c=`${a} is with ${b}`
+console.log(c)
