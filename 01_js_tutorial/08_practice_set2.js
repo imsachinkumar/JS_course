70f56ab40ae20551af274b212793e83d3fbc4c3d
imsachinkumar <122716893+imsachinkumar@users.noreply.github.com>
2024-08-24 06:05:40 +0000

    practice

---
+++ 01_js_tutorial/08_practice_set2.js
@@ -0,0 +1,32 @@
+// ques 1
+
+// let age=13;
+// if(age<54 && age>18){
+//     console.log("right age")
+// }else{
+//     console.log("not right")
+// }
+
+//QUES 2
+
+//checkno.is divisivle by 2 & 3
+
+// const a=9
+// if(a%2==0 && a%3==0){
+//     console.log("yes")
+// }else{
+//     console.log("no")
+// }
+
+//QUES 3
+
+//checkno.is divisivle by 2 or 3
+
+const a=17
+
+if(a%2==0 ||  a%3==0){
+    console.log("yes")
+}else{
+    console.log("no")
+}
+
