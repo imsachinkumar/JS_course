12151e87507abcacb70819a051733d02ef4e9580
imsachinkumar <122716893+imsachinkumar@users.noreply.github.com>
2024-08-24 16:32:34 +0000

    pset

---
+++ 01_js_tutorial/11_practice_set3.js
@@ -0,0 +1,59 @@
+// CONSTRUCT A OBJ THAT PRINT  VALUE OF MARKS OF STUDENTS
+
+
+//USING FOR LOOP
+
+// let marks={
+//     sachin:57,
+//     shagun:65,
+//     sonam:67
+// }
+// for(let i in marks){
+//     console.log(i,marks[i])
+
+// }
+
+
+// USING FOR IN LOOP
+
+
+// let marks={
+//     Sachin:34,
+//     Shuvam:9,
+//     gungun:78
+// }
+// for(let i=0;i<Object.keys(marks).length;i++){
+//     console.log(Object.keys(marks)[i] + " : "+ marks[Object.keys(marks)[i]]);
+
+
+    // Object.keys(marks)[i] will gave value from 0 index to 2 that is Sachin shuvam gungun
+    //marks[Object.keys(marks)[i]] will gave value of that idexes items that is 34 9 78
+
+//}
+
+
+
+//problem 3
+
+
+// let a=23
+// let i
+// while(i!=23){
+//     i=prompt("enter a number")
+// }
+// console.log("you enter right number")
+
+
+//problem 4 
+//write a function to find mean of 5 numbers
+
+// function mean(a,b,c,d,e){
+//     return (a+b+c+d+e)/5
+// }
+// a=3
+// b=4
+// c=4
+// d=9
+// e=10
+
+// console.log(mean(a,b,c,d,e))
