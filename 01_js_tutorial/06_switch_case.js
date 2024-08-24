f2a80c383590500460ccddd6387307c41fd7eeec
imsachinkumar <122716893+imsachinkumar@users.noreply.github.com>
2024-08-24 05:48:43 +0000

    switch case

---
+++ 01_js_tutorial/06_switch_case.js
@@ -0,0 +1,16 @@
+const marks=74;
+
+switch(marks){
+    case 34:
+        console.log("marks are 34");
+        break;
+    case 54:
+        console.log("marks are 54");
+        break;
+    case 75:
+        console.log("Marks are 75");  
+        break;
+    default:
+        console.log("marks not found")   
+   
+}
