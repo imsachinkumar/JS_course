e7e3a43bc987cef8ce5e75a4bca5df1c72f70f40
imsachinkumar <122716893+imsachinkumar@users.noreply.github.com>
2024-08-26 13:56:02 +0000

    array

---
+++ 01_js_tutorial/13_arrays.js
@@ -0,0 +1,72 @@
+//ARRAYS
+
+// let marks=[23,343,545,32,232,null,undefined];
+//  console.log(marks)
+// console.log(marks[3])
+// console.log(marks.length)
+// marks[0]=34//   CHANGING NEW VALUE
+// marks[7]=76// ADDING NEW VALUE
+// console.log(marks)
+// for(let i=0;i<marks.length;i++){
+//     console.log(marks[i])
+// }
+
+
+//ARRAYS METHODS
+
+
+// let arr=[12,2,323,3,5,"sachin"];
+// console.log(arr.toString())
+
+// console.log(arr.toLocaleString())
+
+// console.log(arr.at(2))
+
+// console.log(arr)
+
+// //console.log(arr.join("-"));
+
+// console.log(arr.pop())
+
+// let newarray=(arr.push("hello"))//RETURN NEW ARRAY LENGTH
+// console.log(newarray)
+
+// let arr1=[21,3,23,2,32]
+// let arr2=["sonam","shivam"]
+
+// console.log(arr.concat(arr1,arr2))
+
+
+// let arr3=["sachin","aman","pawan","rohit"]
+
+// let arr4=arr3.sort();// IT SORTS ALPHABATICALLY
+// console.log(arr4)
+
+
+//NUMERICALLLY SORTING 
+
+// let compare=(a,b)=>{
+//    return a-b
+// }
+// let arr5=[23,3,23,3,434,3,44,5];
+// arr5.sort(compare)
+// console.log(arr5)
+
+// arr5.reverse()
+// console.log(arr5)
+
+
+//SLICE
+
+
+// let arr7=[2,3,4,5,1,0,8];
+// console.log(arr7.slice(1,4))
+
+
+
+
+//SPLICE
+
+
+let arr8=[23,3,454,5,6,7,67]
+console.log(arr8.splice(1,5))
