90d397b49959541505e65eebfd6da8b70b584a2a
imsachinkumar <122716893+imsachinkumar@users.noreply.github.com>
2024-08-26 15:56:44 +0000

    ARRAYLOOPS

---
+++ 01_js_tutorial/14_arrays_loops.js
@@ -0,0 +1,19 @@
+
+
+
+// FOR OF
+
+
+let arr=[434314,3,4,133,12,32,32]
+
+for(let i of arr){   // for of give value
+    console.log(i)
+}
+
+//FOR IN
+
+let arr1=[434314,3,4,133,12,32,32]
+
+for(let i in arr1){   // for in give KEYS
+    console.log(i)
+}
