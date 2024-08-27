32f1f5d6b59d1843daa951167484e7b9cbb1c2ee
imsachinkumar <122716893+imsachinkumar@users.noreply.github.com>
2024-08-27 16:15:46 +0000

    map

--- 01_js_tutorial/15_map_filter_reduce.js
+++ 01_js_tutorial/15_map_filter_reduce.js
@@ -1,37 +1,46 @@
 //ARRAY MAP METHOD
 
 
-let arr=[23,4,5,323,45,78]
+// let arr=[23,4,5,323,45,78]
 
-a1=arr.map((value)=>{
-    console.log(value)
+// a1=arr.map((value)=>{
+//     console.log(value)
 
-})
-console.log(a1)
+// })
+// console.log(a1)
 
 
-let arr2=[23,4,5,323,45,78]
+// let arr2=[23,4,5,323,45,78]
 
-a2=arr.map((value,index,array)=>{
-    console.log(value,index,array)
-    return value+index
+// a2=arr.map((value,index,array)=>{
+//     console.log(value,index,array)
+//     return value+index
 
-})
-console.log(a1)
+// })
+// console.log(a1)
 
 
 
 //ARRAY FILTER METHOD
 
-let arr3=[23,45,6,7,9,10]
+// let arr3=[23,45,6,7,9,10]
 
-let a4=arr3.filter((a4)=>{
-    return a4>10
+// let a4=arr3.filter((a4)=>{
+//     return a4>10
 
-})
-console.log(a4)
+// })
+// console.log(a4)
 
 
 
 //REDUCE
+
+
+let arr6=[23,3,4,5,6,7]
+
+let arr7=arr6.reduce((val1,val2)=>{
+    return val1+val2 // 23+3+4+5+6+7
+
+})
+console.log(arr7)
 
